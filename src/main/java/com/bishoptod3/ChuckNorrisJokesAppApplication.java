36ba9d73c3e9d48e76db3fb8287a968af796bc20
Loky <puiualexandru87@yahoo.com>
2018-08-09 19:29:32 +0200

    Added XML Configuration Example

--- src/main/java/com/bishoptod3/ChuckNorrisJokesAppApplication.java
+++ src/main/java/com/bishoptod3/ChuckNorrisJokesAppApplication.java
@@ -2,8 +2,10 @@ package com.bishoptod3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisJokesAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bishoptod3/config/ChuckConfiguration.java
+++ src/main/java/com/bishoptod3/config/ChuckConfiguration.java
@@ -1,16 +1,14 @@
 package com.bishoptod3.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Loky on 09/08/2018.
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
